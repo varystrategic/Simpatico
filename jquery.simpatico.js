4de5acba3b156c66ee7a7642e4ab65212e1ebed1
Andrew Tibbetts <andrewgtibbetts@gmail.com>
2013-04-21 22:42:49 -0500

    Revert "cleaned up anchor options"
    This reverts commit 3787e02d6bf93cf5c8c3efee4d4a485c5b153edd.

--- jquery.simpatico.js
+++ jquery.simpatico.js
@@ -12,7 +12,7 @@ License 	: MIT, GPL
 (function( $ ){
 
 var simpatico_defaults = {
-	'view' : 'fill', // fit or fill
+	'style' : 'fill', // fit or fill
 	'anchor_x' : 'center', // left, center, right
 	'anchor_y' : 'middle' // top, middle, bottom
 };
@@ -21,18 +21,19 @@ $.fn.simpatico = function( opts ) {
 
 	this.each((function(){
 
-		function fit_width(dims,o) {
+		function fit_width(dims) {
 		
 			dims.newW = dims.conW; // set destination width to container width
 			dims.newH = ( dims.conW/dims.imgW ) * dims.imgH; // calculate new image height
 			
 			// calculate top and bottom
-			switch (o.anchor_y) {
+			switch (anchor_y) {
 				case 'top':
 					dims.newT = 0;
+					dims.newB = 'auto';
 						break;
 				case 'middle':
-					switch (o.view) {
+					switch (style) {
 						case 'fit':
 							dims.newT = ( ( dims.conH - dims.newH ) / 2 );
 								break;
@@ -40,30 +41,34 @@ $.fn.simpatico = function( opts ) {
 							dims.newT = ( ( dims.newH - dims.conH ) / 2 ) * -1;
 								break;
 					}
+					dims.newB = 'auto';
 						break;
 				case 'bottom':
-					dims.newT = dims.conH - dims.newH;
+					dims.newT = 'auto';
+					dims.newB = 0;
 						break;
 			}
 		
 			// ignore anchor_x since image will fit horizontally
+			dims.newR = 0;
 			dims.newL = 0;
 			
 			return dims;
 		}
 
-		function fit_height(dims,o) {
+		function fit_height(dims) {
 		
 			dims.newW = ( dims.conH/dims.imgH ) * dims.imgW; // calculate new image width
 			dims.newH = dims.conH; // set destination height to container height
 			
 			// calulate left and right
-			switch (o.anchor_x) {
+			switch (anchor_x) {
 				case 'left':
 					dims.newL = 0;
+					dims.newR = 'auto';
 						break;
 				case 'center':
-					switch (o.view) {
+					switch (style) {
 						case 'fit':
 							dims.newL = ( ( dims.conW - dims.newW ) / 2 );
 								break;
@@ -71,13 +76,16 @@ $.fn.simpatico = function( opts ) {
 							dims.newL = ( ( dims.newW - dims.conW ) / 2 ) * -1;
 								break;
 					}
+					dims.newR = 'auto';
 						break;
 				case 'right':
-					dims.newL = dims.conW - dims.newW;
+					dims.newL = 'auto';
+					dims.newR = 0;
 			}
 			
 			// ignore anchor_y since image will fit vertically
 			dims.newT = 0;
+			dims.newB = 0;
 			
 			return dims;
 		}
@@ -100,6 +108,8 @@ $.fn.simpatico = function( opts ) {
 				newW: 0, // destination width
 				newH: 0, // destination height
 				newT: 0, // destination top
+				newR: 0, // destination right
+				newB: 0, // destination bottom
 				newL: 0  // destination left
 			}
 	
@@ -123,24 +133,23 @@ $.fn.simpatico = function( opts ) {
 					dims.imgW = this.width; // get image width (gotta use js as jquery fails)
 					dims.imgH = this.height; // get image height (gotta use js as jquery fails)
 					
-					switch (o.view) {
+					switch (style) {
 						case 'fit': // show the whole image
-							if ( img_is_landscapier(dims) )
-								fit_width(dims,o);
+							if ( img_is_landscapier() )
+								fit_width(dims);
 							else
-								fit_height(dims,o);
+								fit_height(dims);
 								break;
 						default: // 'fill', leave no empty space in container
-							if ( img_is_landscapier(dims) )
-								fit_height(dims,o);
+							if ( img_is_landscapier() )
+								fit_height(dims);
 							else
-								fit_width(dims,o);
+								fit_width(dims);
 								break;
 					}
-					console.log(o);
-
+	
 					// apply new styles to the image. done!
-					$image.width(dims.newW).height(dims.newH).css({ position: 'absolute', top: dims.newT, left: dims.newL });
+					$image.width(dims.newW).height(dims.newH).css({ position: 'absolute', top: dims.newT, right: dims.newR, bottom: dims.newB, left: dims.newL });
 	
 				});
 			});
